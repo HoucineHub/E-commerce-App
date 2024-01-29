9726870a597afee7f5d7f0ba690a8ba725ca4965
HoucineHub <assadhoucine10@gmail.com>
2024-01-29 00:14:01 +0000

    V2

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Shop from './pages/shop/Shop';
 import Cart from './pages/cart/Cart';
